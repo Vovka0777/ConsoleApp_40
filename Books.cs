f07e60eb6d9ac4d6f926567f394ce72cfd5422bb
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-05-15 11:23:51 +0300

    Add files via upload

---
+++ Books.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41_1
+{
+    class Book
+    {
+        string name;
+        string author;
+        string pubhouse;
+        //List<Book> books = new List<Book>();
+
+        public string Name { get => name; set => name = value; }
+        public string Author { get => author; set => author = value; }
+        public string Pubhouse { get => pubhouse; set => pubhouse = value; }
+        //List<Book> Books { get => books; set => books = value; }
+
+        public Book(string name, string author, string pubhouse /*List<Book> books*/)
+        {
+            this.Name = name;
+            this.Author = author;
+            this.Pubhouse = pubhouse;
+            //this.books = books;
+        }
+
+        public void AddBook ()
+        {
+            Console.WriteLine("Введите название");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите автора");
+            string author = Console.ReadLine();
+            Console.WriteLine("Введите издательство");
+            string pubhouse = Console.ReadLine();
+            books.Add (new Book(name, author, pubhouse));
+        }
+
+        //static void SortBooks (Book Books)
+        //{
+        //    Array.Sort(Book Books);
+        //}
+    }
+}
---
+++ SortBooks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41_1
+{
+    class SortBooks
+    {
+        List<Book> books = new List<Book>();
+
+        internal List<Book> Books { get => books; set => books = value; }
+
+        public SortBooks(List<Book> books) { this.Books = books; }
+
+        public void SortB(SortBooks comparer)
+        {
+            Array.Sort(books,comparer);
+        }
+    }
+}
